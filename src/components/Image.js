4c43d754819521f38943a2af1d9f7d52a98eb504
abflett <abflett@yahoo.ca>
2019-02-03 11:05:36 -0600

    Updated webpage.

---
+++ src/components/Image.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class Image extends Component {
+  render() {
+    return (
+      <div style={{
+          width: 100
+      }}>
+        
+      </div>
+    );
+  }
+}
+
+export default Image;
